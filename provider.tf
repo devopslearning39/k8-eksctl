950362d4d7ae851ec5f4350569a9ce1eb2d9fc29
anil <devopslearning39>
2025-06-29 18:54:00 +0530

    commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0" # AWS provider version, not terraform version
+    }
+  }
+
+  backend "s3" {
+    bucket = "praveen-k8-practice"
+    key    = "eksctl"
+    region = "us-east-1"
+    dynamodb_table = "jp-k8-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
